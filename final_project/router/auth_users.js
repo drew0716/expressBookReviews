a451577fdd1774dce9906931656e362bfcbd82d0
Drew Galbraith <drew0716@gmail.com>
2025-01-25 16:59:25 -0500

    Task 8 complete

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -59,8 +59,27 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn; //Retrieve ISBN
+  let book = books[isbn]; // Find the book
+  const username = req.session.authorization?.username; //Get the logged in username
+
+  //Check if user is logged in
+  if (!username) {
+    return res.status(401).json({ message: "User not logged in or session expired." });
+  }
+
+  //Check if book exists
+  if (book) {
+    let review = req.body.review;
+    if (review) {
+        book.reviews[username] = review;
+        return res.status(200).json({ message: "Review successfully added/updated!"});
+    } else {
+        return res.status(400).json({ message: "Review content is required."});
+    }
+  }
+
+  return res.status(404).json({ message: "Book not found" });
 });
 
 module.exports.authenticated = regd_users;
