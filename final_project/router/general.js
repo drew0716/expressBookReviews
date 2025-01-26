0e1aaf2702977d0e017bd84ace2f2311c2ea34c6
Drew Galbraith <drew0716@gmail.com>
2025-01-25 19:13:47 -0500

    Task 11 complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -43,19 +43,27 @@ public_users.get('/', async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-  //Retrieve the ISBN from the route parameter
-  const isbn = req.params.isbn;
-  //Check if the book exists in the books database
-  if (books[isbn]) {
-    return res.status(200).json(books[isbn]); //Return book details
-  } else {
-    return res.status(404).json({ message: "Book not found" }); //Return an error
-  }
+public_users.get('/isbn/:isbn', async function (req, res) {
+    try {
+        //Retrieve the ISBN from the route parameter
+        const isbn = req.params.isbn;
+        //Get book data asynchronously using axios
+        const response = await axios.get('http://localhost:5000/api/books');
+        const books = response.data;
+        //Check if the book exists
+        if (books[isbn]) {
+            return res.status(200).json(books[isbn]); //Return book details
+          } else {
+            return res.status(404).json({ message: "Book not found" }); //Return an error
+        }
+    } catch (error) {
+         //Return error if unable to get books data
+         return res.status(500).json({ message: `Error fetching books: ${error}` });       
+    }
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', function (req, res) {
   //Retrieve the author from the route parameter
   const author = req.params.author;
   //Filter the books array to find matching authors based on parameter
